ab17f2fd808a2404b8936dd74ad7ff6c9989f488
Cobra30621 <cobra20621@gmail.com>
2021-01-20 17:15:25 +0800

    以Google sheet 寫好排行榜的程式 接下來要新增讀取中等等

---
+++ Assets/Scripts/Web/GoogleSheetManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GoogleSheetManager : MonoBehaviour
+{
+    private string url = "https://script.google.com/macros/s/AKfycby8paaFVRQzx804FeOgy7d6BDfrQC1Er0tDLD9LElJuiJYH7_pRKq4T/exec";
+    private string method;
+    private string id;
+    private string json;
+
+
+    void Awake()
+    {
+        SaveData("3倍數", "Json");
+        LoadData("2的倍數");
+        
+    }
+
+    public void SaveData(string id, string json){
+        this.method = "write";
+        this.id = id;
+        this.json = json;
+        StartCoroutine(Upload());
+    }
+
+    public void LoadData(string id){
+        this.method = "read";
+        this.id = id;
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("method", method);
+        form.AddField("id", id);
+        form.AddField("json", json);
+
+        using (UnityWebRequest www = UnityWebRequest.Post(url , form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                print(www.downloadHandler.text);
+                Debug.Log("Form upload complete!");
+                if(method == "read"){
+                    json = www.downloadHandler.text;
+                    OnLoadComplete();
+                }
+                    
+                
+            }
+        }
+    }
+
+    public void OnLoadComplete(){
+        Debug.Log($"Load:{json}");
+    }
+}
